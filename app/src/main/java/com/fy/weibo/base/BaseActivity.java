4fa94f9b9b8150b42bc1b556e37f88de1282c426
WwinnerFan <15310610871@163.com>
2018-09-14 19:43:16 +0800

    添加

--- app/src/main/java/com/fy/weibo/base/BaseActivity.java
+++ app/src/main/java/com/fy/weibo/base/BaseActivity.java
@@ -1,7 +1,23 @@
 package com.fy.weibo.base;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    private long firstTime = 0;
+
+    @Override
+    public void onBackPressed() {
+        long secondTime = System.currentTimeMillis();
+        if (secondTime - firstTime > 2000) {
+            Toast.makeText(BaseActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            firstTime = secondTime;
+        } else{
+            finish();
+        }
+    }
+
+
 }
